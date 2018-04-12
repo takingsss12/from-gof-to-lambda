9c1bf331c47c48bc174de2665f4d99b934abfda3
takingsss12 <takingsss12@gmail.com>
2018-04-12 17:20:46 +0100

    removed duplication in ChainOfRespLambda#parse...(File file)

--- src/main/java/org/mfusco/fromgoftolambda/examples/chainofresponsibility/ChainOfRespLambda.java
+++ src/main/java/org/mfusco/fromgoftolambda/examples/chainofresponsibility/ChainOfRespLambda.java
@@ -4,46 +4,49 @@ import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
-public class ChainOfRespLambda {
+import org.mfusco.fromgoftolambda.examples.chainofresponsibility.File.Type;
 
-    public static Optional<String> parseText(File file) {
-        return Optional.ofNullable(file)
-          .filter(f -> f.getType() == File.Type.TEXT )
-          .map(f -> "Text file: " + f.getContent());
-    }
+public class ChainOfRespLambda
+{
 
-    public static Optional<String> parsePresentation(File file) {
-        return Optional.ofNullable(file)
-          .filter(f -> f.getType() == File.Type.PRESENTATION )
-          .map(f -> "Presentation file: " + f.getContent());
-    }
+  public static Optional<String> parseFile(File file, Type type)
+  {
+    return Optional.ofNullable(file)
+        .filter(fileToFilter -> type.equals(fileToFilter.getType()))
+        .map(Object::toString);
+  }
 
-    public static Optional<String> parseAudio(File file) {
-        return Optional.ofNullable(file)
-          .filter(f -> f.getType() == File.Type.AUDIO )
-          .map(f -> "Audio file: " + f.getContent());
-    }
+  public static Optional<String> parseText(File file)
+  {
+    return parseFile(file, Type.TEXT);
+  }
 
-    public static Optional<String> parseVideo(File file) {
-        return Optional.ofNullable(file)
-          .filter(f -> f.getType() == File.Type.VIDEO )
-          .map(f -> "Video file: " + f.getContent());
-    }
+  public static Optional<String> parsePresentation(File file)
+  {
+    return parseFile(file, Type.PRESENTATION);
+  }
 
-    public static void main( String[] args ) {
-        File file = new File( File.Type.AUDIO, "Dream Theater  - The Astonishing" );
+  public static Optional<String> parseAudio(File file)
+  {
+    return parseFile(file, Type.AUDIO);
+  }
 
-        System.out.println(
-        Stream.<Function<File, Optional<String>>>of(
-                ChainOfRespLambda::parseText,
-                ChainOfRespLambda::parsePresentation,
-                ChainOfRespLambda::parseAudio,
-                ChainOfRespLambda::parseVideo )
-                .map(f -> f.apply( file ))
-                .filter( Optional::isPresent )
-                .map( Optional::get )
-                .findFirst()
-                .orElseThrow( () -> new RuntimeException( "Unknown file: " + file ) )
-        );
-    }
+  public static Optional<String> parseVideo(File file)
+  {
+    return parseFile(file, Type.VIDEO);
+  }
+
+  public static void main(String[] args)
+  {
+    File file = new File(File.Type.AUDIO, "Dream Theater  - The Astonishing");
+
+    System.out.println(Stream
+        .<Function<File, Optional<String>>>of(ChainOfRespLambda::parseText, ChainOfRespLambda::parsePresentation,
+            ChainOfRespLambda::parseAudio, ChainOfRespLambda::parseVideo)
+        .map(f -> f.apply(file))
+        .filter(Optional::isPresent)
+        .map(Optional::get)
+        .findFirst()
+        .orElseThrow(() -> new RuntimeException("Unknown file: " + file)));
+  }
 }
--- src/main/java/org/mfusco/fromgoftolambda/examples/chainofresponsibility/File.java
+++ src/main/java/org/mfusco/fromgoftolambda/examples/chainofresponsibility/File.java
@@ -1,27 +1,42 @@
 package org.mfusco.fromgoftolambda.examples.chainofresponsibility;
 
-public class File {
+public class File
+{
 
-    enum Type { TEXT, PRESENTATION, AUDIO, VIDEO }
-
-    private final Type type;
-    private final String content;
-
-    public File( Type type, String content ) {
-        this.type = type;
-        this.content = content;
-    }
-
-    public Type getType() {
-        return type;
-    }
-
-    public String getContent() {
-        return content;
-    }
+  enum Type
+  {
+    TEXT, PRESENTATION, AUDIO, VIDEO;
 
     @Override
-    public String toString() {
-        return type + ": " + content;
+    public String toString()
+    {
+      return name().charAt(0) + name().substring(1)
+          .toLowerCase();
     }
+  }
+
+  private final Type type;
+  private final String content;
+
+  public File(Type type, String content)
+  {
+    this.type = type;
+    this.content = content;
+  }
+
+  public Type getType()
+  {
+    return type;
+  }
+
+  public String getContent()
+  {
+    return content;
+  }
+
+  @Override
+  public String toString()
+  {
+    return type + " file: " + content;
+  }
 }
